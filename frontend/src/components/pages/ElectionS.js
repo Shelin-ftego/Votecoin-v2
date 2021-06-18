6d009ecb654ccc3b960c274636ffd532428235d6
Shelin-ftego <shelin.ftego.003@gmail.com>
2021-06-18 15:35:18 +0200

    election  status page update
    uses component class

--- frontend/src/components/pages/ElectionS.js
+++ frontend/src/components/pages/ElectionS.js
@@ -1,18 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import NavbarA from '../NavbarA';
 
+//WEB3 still needs to go here
+//initialization for web3
 
-function ElectionS() {
-    const [print,setPrint] = useState(null);
+class ElectionS extends Component{
+    //states still need to be defined
+    handleStartSubmit = event => {
+        console.log("start")
+        event.preventDefault()
+        //call web 3
+        };
 
+    handleStopSubmit = event => {
+        event.preventDefault()
+        console.log("stop")
+        //call web3
+        };
+
+    render(){
     return (
         <div>
             <NavbarA/>
-        <h1> CURRENT VOTING STATUS: </h1>
-        <button onClick={()=>setPrint(true)}>Start Voitng Period</button>
-        <button onClick={()=>setPrint(false)}>End Voting Period</button>
+        <h1> VOTING STATUS: </h1>
+        <button onClick={this.handleStartSubmit}>Start Voitng Period</button>
+        <button onClick={this.handleStopSubmit}>End Voting Period</button>
         </div>
     )
+    }
+
 }
 
 export default ElectionS

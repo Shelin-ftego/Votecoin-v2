335634fc7301426d2fece4ac585cd5b92663d0df
Siven25 <siven.naicker@gmail.com>
2021-06-19 18:06:53 +0200

    Revert "Support and about"
    This reverts commit 90d88e25f1bda1b242dbf25d9aa6b22ecb6c8a0d.

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../App.css';
 // import Footer from '../Footer';
 import Navbar from '../Navbar';
-
+//import Footer from '../Footer';
 
 export default function About() {
   const aboutstyle={

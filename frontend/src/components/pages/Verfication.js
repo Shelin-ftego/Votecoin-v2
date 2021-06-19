b71fe30ac6dd1a5ea188b56b5706199f7cd76255
Shelin-ftego <shelin.ftego.003@gmail.com>
2021-06-19 21:22:51 +0200

    change verification
    uses component style now

--- frontend/src/components/pages/Verfication.js
+++ frontend/src/components/pages/Verfication.js
@@ -1,47 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, Component} from 'react';
 import NavbarV from '../NavbarV';  
 import { Grid,Paper, Avatar, TextField, Button, Typography } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 
-function Verfication() {
-    const [data,setData] = useState(null);
-    const [print,setPrint] = useState(null);
+class Verfication extends Component{
+    state = {data:undefined, print:undefined}
+    paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
+    avatarStyle={backgroundColor:'#1bbd7e'}
+    btnstyle={margin:'8px 0'}
 
-    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#1bbd7e'}
-    const btnstyle={margin:'8px 0'}
+    getData(val)
+    {
+        this.setState({data:val})
+        console.warn(val.target.value)
+    } 
 
-
-  function getData(val)
- {
-     setData(val.target.value)
-     console.warn(val.target.value)
- } 
-
+    render(){
     return (
         <div>
             <NavbarV/>
             {
-                print?
+                this.state.print?
                 <h1>{data}</h1>
                 :null
             }
-            <h1>{data}</h1>
+            <h1>{this.state.data}</h1>
             <Grid>
-            <Paper elevation={10} style={paperStyle}>
+            <Paper elevation={10} style={this.paperStyle}>
             <Grid align='center'>
-                   <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                   <Avatar style={this.avatarStyle}><LockOutlinedIcon/></Avatar>
                <h2>Check Registration:</h2>
               </Grid>
-            <TextField label='Voter Number:' placeholder='Please enter Voter Number:' onChange={getData}/>
+            <TextField label='Voter Number:' placeholder='Please enter Voter Number:' onChange={this.setState({data:'what is this for'})}/>
             <br/>
             <br/>
-            <Button type='submit' color='primary' variant="contained" onClick={()=>setPrint(true)} style={btnstyle}>Check Voter number: </Button>
+            <Button type='submit' color='primary' variant="contained" onClick={()=>this.setState({print:true})} style={btnstyle}>Check Voter number: </Button>
             </Paper>
             </Grid>
         </div>
     )
+    }
 }
 
 export default Verfication

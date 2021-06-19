a62fc0d1eb9366629c67813beeb66e584ba6e694
Michael Paulinus <michaelpaulinus@gmail.com>
2021-06-20 00:06:00 +0200

    Added web3

--- frontend/src/components/pages/Results.js
+++ frontend/src/components/pages/Results.js
@@ -46,41 +46,64 @@ const useStyles = makeStyles({
 
 class Results extends Component{
 
-    // states + web3 states
-    state = { results: undefined, web3: null, accounts: null, contract: null, status: null };
+  // states + web3 states
+  state = { results: undefined, web3: null, accounts: null, contract: null, status: null };
 
-    // web3 initialization
-    componentDidMount = async () => {
-      try {
-        // Get network provider and web3 instance.
-        const web3 = await getWeb3();
-  
-        // Use web3 to get the user's accounts.
-        const accounts = await web3.eth.getAccounts();
-  
-        // Get the contract instance.
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = ElectionContract.networks[networkId];
-        const instance = new web3.eth.Contract(
-          ElectionContract.abi,
-          deployedNetwork && deployedNetwork.address,
-        );
-  
-        console.log(networkId);
-        console.log(deployedNetwork.address);
-        console.log(ElectionContract.abi);
-        
-        // Set web3, accounts, and contract to the state, and then proceed with an
-        // example of interacting with the contract's methods.
-        this.setState({ web3, accounts, contract: instance }, this.fetchStatus);
-      } catch (error) {
-        // Catch any errors for any of the above operations.
-        alert(
-          `Failed to load web3, accounts, or contract. Check console for details.`,
-        );
-        console.error(error);
+  // web3 initialization
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = ElectionContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        ElectionContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      console.log(networkId);
+      console.log(deployedNetwork.address);
+      console.log(ElectionContract.abi);
+      
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.fetchResults);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  // get results from blockchain
+  fetchResults = async () => {
+    const { accounts, contract } = this.state;
+    // const winner = await contract.methods.getWinnerCandidate().call(); // first get winner candidate
+    // console.log(winner);
+
+    const totalVotes = await contract.methods.totalVotes().call(); // get total votes
+
+    // index to get the last candidate in array
+    var index = (await contract.methods.getNumofCandidates().call()) - 1;
+    console.log(index);
+
+    if (index>=0){
+      // getCandidate is a method which returns 3 values, by default these get stored in an array, so "response" is an array
+      for (var i=0; i<(index-1); i++){
+        const response = await contract.methods.getCandidate(i).call();
+        // store response[1] into table // party name
+        // store response[2] into table // votes received
+        // % votes = response[2]/totalVotes * 100 // % of votes received
       }
-    };  
+    }
+  };    
 
   createData(name, votes, totalvotes) {
     return { name, votes, totalvotes };

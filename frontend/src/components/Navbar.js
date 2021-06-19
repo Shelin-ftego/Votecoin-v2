e344ec6b9c18038ed06d85c9c47dc68f17550b17
Michael Paulinus <michaelpaulinus@gmail.com>
2021-06-19 16:50:45 +0200

    Login button updated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -58,7 +58,7 @@ function Navbar() {
                          </Link> 
                      </li>
                  </ul>
-                 {button && <Button buttonStyle='btn--outline'>LOG IN</Button>}
+                 {button && <Button buttonStyle='btn--outline'>LOGIN</Button>}
              </div>  
          </nav>   
         </>
